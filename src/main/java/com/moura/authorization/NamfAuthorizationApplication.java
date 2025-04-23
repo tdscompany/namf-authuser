64e518f47dde32035142b9b4c6204c10265ecfc7
Thiago Paiva <thiago.paiva@tds.company>
2025-04-23 14:45:48 -0300

    first commit

---
+++ src/main/java/com/moura/authorization/NamfAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.moura.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamfAuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamfAuthorizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moura/authorization/NamfAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moura.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamfAuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
